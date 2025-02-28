db19e1854bfee28dbf1a4ea92186707beb8a44dd
Jeanne4L <sandra.petereau@outlook.fr>
2025-02-28 19:42:34 +0100

    fix(page): create page container + stylized scrollbar + update favicon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,14 @@
 import { withHeader } from "../../hoc/withHeader"
+import PageContainer from "../parts/PageContainer"
 import H1 from "../../components/text/H1"
 import Form from "./Form"
 
 const HomePage = () => {
   return (
-    <>
+    <PageContainer>
       <H1 text='New employee' uppercase />
       <Form />
-    </>
+    </PageContainer>
   )
 }
 
---
+++ src/pages/parts/PageContainer/styles/index.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled"
+
+import { Margins } from "../../../../constants/margins"
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${Margins.M10};
+`
