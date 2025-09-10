bc40f5a9f19bc7c806dd61b05200185aff9a9cb4
Jeanne4L <sandra.petereau@outlook.fr>
2025-09-10 11:02:53 +0200

    refactor(withHeader): replace hoc with layout component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,17 @@
-import { withHeader } from "../../hoc/withHeader"
-import PageContainer from "../parts/PageContainer"
+import Layout from "../../layouts/Layout"
 import H1 from "../../components/text/H1"
+import PageContainer from "../parts/PageContainer"
 import Form from "./Form"
 
 const HomePage = () => {
   return (
-    <PageContainer>
-      <H1 text='New employee' uppercase />
-      <Form />
-    </PageContainer>
+    <Layout>
+      <PageContainer>
+        <H1 text='New employee' uppercase />
+        <Form />
+      </PageContainer>
+    </Layout>
   )
 }
 
-export default withHeader(HomePage)
+export default HomePage
